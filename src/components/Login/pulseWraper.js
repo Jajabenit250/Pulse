ffb03aa8c7588e425f023529f0866db94116dfd2
mujohn26 <mujohn26@gmail.com>
2019-11-14 15:20:16 +0200

    feature(sign-up):add signup page
    Both engineer and LF must be able to signup
    
    [start #169649705]

--- src/components/Login/pulseWraper.js
+++ src/components/Login/pulseWraper.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Login from './login';
 import PulseDesign from './pulseDesign';
 import './Login.css';
-// import Signup from './signUp';  
+import Signup from '../signup/Signup';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -13,7 +13,7 @@ render() {
               
                   <Route path="/" ><PulseDesign /> </Route>
                  <Route path="/" exact><Login /> </Route>
-                 {/* <Route path="/signup"  exact  ><Signup /> </Route>  */}
+                 <Route path="/signup"  exact  ><Signup /> </Route>
               {/* 
               NOTE: sign up is commented because doesn't exist
                  */}
