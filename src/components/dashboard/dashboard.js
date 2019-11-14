a80bca39446fc3d1e96967a02c361a5fe486a852
Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>
2019-11-14 17:50:35 +0200

    styles

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SideMenu from './SideMenu';
 import MainMenu from './MainMenu';
 import SearchBox from './SearchBox';
 
-export class Dashboard extends Component {
-    render() {
-        return (
-            <div className="menus">
-                <SideMenu />
-                <MainMenu />
-                <SearchBox />
-            </div>
-        )
-    }
+function Dashboard() {
+  return (
+    <div className="menus">
+      <SideMenu />
+      <MainMenu />
+      <SearchBox />
+    </div>
+  );
 }
 
 export default Dashboard;
