d968eabe5c6d5c904d30376ff20aef7ae4e5a9aa
Jordan Kayinamura <jordankayinamura@gmail.com>
2019-11-24 20:33:08 +0200

    ft(dashboard): calculate each attribute's average

--- src/components/dashboard/MainMenu.js
+++ src/components/dashboard/MainMenu.js
@@ -9,7 +9,7 @@ class MainMenu extends Component {
 
     const loopThroughPeriod = periods => {
       return periods.map((period, index) => {
-        return (<div style={{cursor: "pointer"}} className={period.competed ? "completed" : "not_completed"} onClick={() => this.props.onClick(index)} key={index}>{1 + index}</div>);
+        return (<div style={{cursor: "pointer"}} className={period.completed ? "completed" : "not_completed"} onClick={() => this.props.onClick(index)} key={index}>{1 + index}</div>);
       });
     };
 
@@ -20,9 +20,12 @@ class MainMenu extends Component {
         <h1>{this.props.title}</h1>
         <span>30.11.2019 - 2.01.2020</span>
         <p className="badge">{this.props.user.type}</p>
+        <div className="averages">{this.loopTHroughAverages()}</div>
+        <div className="container__circle">
         <div className="line"></div>
         <div className="circle">
           {loopThroughPeriod(weeks)}
+        </div>
         </div>
         <div
           className="average"
@@ -37,6 +40,16 @@ class MainMenu extends Component {
       </div>
     );
   }
+   loopTHroughAverages = () => {
+      if (this.props.each) {
+         const attributes = ['Quality', 'Quantity', 'Initiative', 'Professionalism', 'Communication', 'Integration'];
+       return this.props.each.map((average, index) => {
+            return <div  className="attribute__average" style={{display: this.props.user.type === 'Software Engineer' ? "inline" : "none", border: average <= 1 ? "1px solid #CD5C5C" : "1px solid #2E8B57", color: average <= 1 ? "#CD5C5C" : "#2E8B57"}} key= {index}>{attributes[index] + ": "+ average}</div>
+        });
+         
+      }
+   }
+  
 }
 
 export default MainMenu;
