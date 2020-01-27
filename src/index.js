d17cc33d3f04dab67b6f84d74e6cbb993a0e7312
Veda <nsengimanavedadom@gmail.com>
2020-01-27 12:46:04 +0200

    This add material-ui and remove other packages (#24)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import {  Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store/store';
 
 
@@ -10,4 +10,4 @@ ReactDOM.render(
    <Provider store={store}>
       <App />
    </Provider>, document.getElementById('root')
-   );
+);
